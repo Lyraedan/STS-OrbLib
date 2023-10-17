8b46ddffbea31a67baf970a00d3a5fc9c8aed809
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2023-10-18 00:25:30 +0100

    Fixed bug with essence of darkness

--- orbLib/src/main/java/orbLib/patches/potions/DefectPotionEssenceOfDarknessPatch.java
+++ orbLib/src/main/java/orbLib/patches/potions/DefectPotionEssenceOfDarknessPatch.java
@@ -4,11 +4,9 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.EssenceOfDarkness;
-import com.megacrit.cardcrawl.potions.PotionOfCapacity;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import orbLib.actions.DefectEssenceOfDarknessPatchedAction;
